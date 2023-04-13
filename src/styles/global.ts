e37685533dd82872035c53daa1a4e66cba12f432
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-13 17:36:56 -0300

    chore: Configurações inicias & Tela de Login

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+
+  html: {
+    fontSize: "62.5%",
+  },
+
+  body: {
+    backgroundColor: "$gray800",
+    color: "$gray100",
+    "-webkit-font-smoothing": "antialiased",
+  },
+
+  "body, input, textarea, button": {
+    fontFamily: "Nunito, sans-serif",
+    fontWeight: 400,
+    fontSize: "1.6rem",
+  },
+
+  button: {
+    cursor: "pointer",
+  },
+});
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,66 @@
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+import { styled } from "..";
+
+export const HomeContainer = styled("div", {
+  display: "flex",
+  height: "100vh",
+  width: "100vw",
+});
+
+export const HeroContainer = styled("div", {
+  maxWidth: "59.8rem",
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "$purple200",
+  borderRadius: "$md",
+  margin: "2rem",
+});
+
+export const LoginContainer = styled("section", {
+  display: "flex",
+  flex: "1",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+export const LoginContent = styled("form", {
+  display: "flex",
+  maxWidth: "37.2rem",
+  width: "100%",
+  flexDirection: "column",
+
+  "> strong": {
+    fontWeight: "bold",
+    fontSize: "2.4rem",
+  },
+
+  "> p": {
+    marginTop: "0.4rem",
+    marginBottom: "6.4rem",
+  },
+
+  button: {
+    all: "unset",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: "2rem",
+    background: "$gray600",
+    padding: "2rem 2.4rem",
+    borderRadius: "8px",
+    fontSize: "1.8rem",
+    fontWeight: "bold",
+    cursor: "pointer",
+    transition: "all 300ms ease-in-out",
+
+    "&:hover": {
+      filter: "brightness(0.8)",
+    },
+
+    "& + button": {
+      marginTop: "1.6rem",
+    },
+  },
+});
