d3f595810f9929065f91734108e6673666c68804
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-20 10:07:16 -0300

    :boom: fix: Remoção de useSession do componente

--- src/components/LastReadings/index.tsx
+++ src/components/LastReadings/index.tsx
@@ -3,18 +3,17 @@ import { CaretRight } from "phosphor-react";
 import { LastReadBookCard } from "../LastReadBookCard";
 import { IRating } from "@/@types/IRatings";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+
 interface LastReadingsProps {
   ratings: IRating[];
 }
 export function LastReadings({ ratings }: LastReadingsProps) {
-  const session = useSession();
   return (
     <LastReadingsContainer>
       <header>
         <p>Sua última leitura</p>
 
-        <Link href={`/profile/${session.data?.user.id}`}>
+        <Link href={"/profile"}>
           Ver todos
           <CaretRight size={16} weight="bold" />
         </Link>
