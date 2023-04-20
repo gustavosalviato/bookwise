6d90c2ad3252f0f6e708bc75bbe1b112151b2524
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-20 08:34:58 -0300

    :sparkles: feat: Renderizando os dados na página de perfil

--- src/pages/profile/[slug].tsx
+++ src/pages/profile/[slug].tsx
@@ -21,10 +21,12 @@ import { InputText } from "@/components/InputText";
 import { BookOpen } from "phosphor-react";
 import { api } from "@/libs/axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import { RatingScore } from "@/components/RatingScore/styles";
+import { Books, UserList, BookmarkSimple } from "phosphor-react";
 
 interface IProfile {
-  id: string;
   name: string;
+  image: string;
   readed_books: number;
   total_readed_pages: number;
   ratings: Array<{
@@ -46,6 +48,8 @@ interface ProfileProps {
   profileDetails: IProfile;
 }
 export default function Profile({ profileDetails }: ProfileProps) {
+  const { name, ratings, readed_books, total_readed_pages, image } =
+    profileDetails;
   return (
     <ProfileContainer>
       <ProfileScreenShape>
@@ -59,50 +63,37 @@ export default function Profile({ profileDetails }: ProfileProps) {
           <InputText />
 
           <MainSection>
-            <BookCard>
-              <p>Há 2 dias</p>
-              <Card>
-                <CardHeader>
-                  <Image
-                    src="/books/entendendo-algoritmos.png"
-                    alt=""
-                    width={98}
-                    height={134}
-                    quality={100}
-                  />
+            {ratings.map((rating) => (
+              <BookCard key={rating.id}>
+                <p>{rating.formattedDate}</p>
+                <Card>
+                  <CardHeader>
+                    <Image
+                      src={rating.book.cover_url}
+                      alt=""
+                      width={98}
+                      height={134}
+                      quality={100}
+                    />
 
-                  <CardDetails>
-                    <strong>Entendendo Algoritmos</strong>
-                    <p>Aditya Bhargava</p>
+                    <CardDetails>
+                      <strong>{rating.book.name}</strong>
+                      <p>{rating.book.author}</p>
 
-                    {/* <RatingScore /> */}
-                  </CardDetails>
-                </CardHeader>
-                <CardContent>
-                  Tristique massa sed enim lacinia odio. Congue ut faucibus nunc
-                  vitae non. Nam feugiat vel morbi viverra vitae mi. Vitae
-                  fringilla ut et suspendisse enim suspendisse vitae. Leo non
-                  eget lacus sollicitudin tristique pretium quam. Mollis et
-                  luctus amet sed convallis varius massa sagittis. Proin sed
-                  proin at leo quis ac sem. Nam donec accumsan curabitur amet
-                  tortor quam sit. Bibendum enim sit dui lorem urna amet elit
-                  rhoncus ut. Aliquet euismod vitae ut turpis. Aliquam amet
-                  integer pellentesque.
-                </CardContent>
-              </Card>
-            </BookCard>
+                      <RatingScore rating={rating.rate} />
+                    </CardDetails>
+                  </CardHeader>
+                  <CardContent>{rating.book.summary}</CardContent>
+                </Card>
+              </BookCard>
+            ))}
           </MainSection>
         </PageTitleSection>
 
         <AnalyticsSection>
           <CardAnalytics>
-            <img
-              src="https://github.com/gustavosalviato.png"
-              alt=""
-              width={72}
-              height={72}
-            />
-            <strong>Cristofer Rosser</strong>
+            <img src={image} alt="" width={72} height={72} />
+            <strong>{name}</strong>
             <p>membro desde 2019</p>
 
             <Divider />
@@ -110,16 +101,24 @@ export default function Profile({ profileDetails }: ProfileProps) {
             <AnalyticsDetails>
               <BookOpen size={32} color="#50B2C0" />
               <div>
-                <strong>3853</strong>
+                <strong>{total_readed_pages}</strong>
                 <p>Páginas lidas</p>
               </div>
             </AnalyticsDetails>
 
             <AnalyticsDetails>
-              <BookOpen size={32} color="#50B2C0" />
+              <Books size={32} color="#50B2C0" />
               <div>
-                <strong>3853</strong>
-                <p>Páginas lidas</p>
+                <strong>{readed_books}</strong>
+                <p>Livros avaliados</p>
+              </div>
+            </AnalyticsDetails>
+
+            <AnalyticsDetails>
+              <UserList size={32} color="#50B2C0" />
+              <div>
+                <strong>{ratings.length}</strong>
+                <p>Autores lidos</p>
               </div>
             </AnalyticsDetails>
           </CardAnalytics>
